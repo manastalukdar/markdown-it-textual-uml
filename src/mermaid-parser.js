9165aa540ed2a09b5b2667c5589e0fa220c95164
Manas Talukdar <manas.talukdar@gmail.com>
2019-08-31 14:27:40 -0700

    updates.

--- src/mermaid-parser.js
+++ src/mermaid-parser.js
@@ -2,15 +2,21 @@ import mermaid from 'mermaid'
 
 const mermaidChart = (code) => {
   try {
+    /*
     mermaid.parse(code)
+    return `<div class="mermaid">${code}</div>`
+    */
+    var needsUniqueId = "render" + (Math.floor(Math.random() * 10000)).toString();
+    Mermaid.mermaidAPI.render(needsUniqueId, code, sc => {code=sc})
     return `<div class="mermaid">${code}</div>`
   } catch ({ str, hash }) {
     return `<pre>${str}</pre>`
   }
 }
 
-const MermaidParser = (md) => {
-  md.mermaid = mermaid
+const MermaidParser = (md, opts) => {
+  Mermaid.initialize(Object.assign(MermaidPlugIn.default, opts));
+  /* md.mermaid = mermaid
   mermaid.loadPreferences = (preferenceStore) => {
     let mermaidTheme = preferenceStore.get('mermaid-theme')
     if (mermaidTheme === undefined) {
@@ -32,20 +38,33 @@ const MermaidParser = (md) => {
       'mermaid-theme': mermaidTheme,
       'gantt-axis-format': ganttAxisFormat
     }
-  }
+  } */
 
-  const temp = md.renderer.rules.fence.bind(md.renderer.rules)
+  const defaultRenderer = md.renderer.rules.fence.bind(md.renderer.rules);
   md.renderer.rules.fence = (tokens, idx, options, env, slf) => {
     const token = tokens[idx]
-    const code = token.content.trim()
+    // const code = token.content.trim()
+    const code = `${token.info} \n ${token.content.trim()}`
     if (token.info === 'mermaid') {
       return mermaidChart(code)
     }
-    const firstLine = code.split(/\n/)[0].trim()
+    if (token.info === 'mermaid' || token.info === 'gantt' || token.info === 'sequenceDiagram' || token.info.match(/^graph (?:TB|BT|RL|LR|TD);?$/)) {
+      return MermaidChart(code)
+    }
+    /* const firstLine = code.split(/\n/)[0].trim()
     if (firstLine === 'gantt' || firstLine === 'sequenceDiagram' || firstLine.match(/^graph (?:TB|BT|RL|LR|TD);?$/)) {
       return mermaidChart(code)
-    }
+    } */
     return temp(tokens, idx, options, env, slf)
+  }
+
+  MermaidParser.default={
+    startOnLoad: false,
+    theme: "default",
+    flowchart:{
+      htmlLabels: false,
+      useMaxWidth: true,
+    }
   }
 }
 
---
+++ test/index.js
@@ -0,0 +1,59 @@
+import assert from 'assert';
+import markdownIt from 'markdown-it';
+import markdownItMermaid from '../src';
+
+const mdi = markdownIt()
+mdi.use(markdownItMermaid);
+
+assert(mdi.render('# Hello world').trim() === '<h1>Hello world</h1>', '# Hello world')
+assert(mdi.render('Hello world').trim() === '<p>Hello world</p>', 'Hello world')
+
+console.log(mdi.render(`\`\`\`mermaid
+graph TD
+    A[Christmas] -->|Get money| B(Go shopping)
+    B --> C{Let me think}
+    C -->|One| D[Laptop]
+    C -->|Two| E[iPhone]
+    C -->|Three| F[Car]
+\`\`\``))
+// console.log(mdi.render(`\`\`\`
+// graph TD
+//     A[Christmas] -->|Get money| B(Go shopping)
+//     B --> C{Let me think}
+//     C -->|One| D[Laptop]
+//     C -->|Two| E[iPhone]
+//     C -->|Three| F[Car]
+// \`\`\``))
+// console.log(mdi.render(`\`\`\`
+// graph TD
+//     A[Christmas] -->|Get money| B(Go shopping)
+//     B ----> C{Let me think}
+//     C -->|One| D[Laptop]
+//     C -->|Two| E[iPhone]
+//     C -->|Three| F[Car]
+// \`\`\``))
+
+// assert(mdi.render(`\`\`\`mermaid
+// graph TD
+//     A[Christmas] -->|Get money| B(Go shopping)
+//     B --> C{Let me think}
+//     C -->|One| D[Laptop]
+//     C -->|Two| E[iPhone]
+//     C -->|Three| F[Car]
+// \`\`\``).indexOf('class="mermaid"') > -1)
+// assert(mdi.render(`\`\`\`
+// graph TD
+//     A[Christmas] -->|Get money| B(Go shopping)
+//     B --> C{Let me think}
+//     C -->|One| D[Laptop]
+//     C -->|Two| E[iPhone]
+//     C -->|Three| F[Car]
+// \`\`\``).indexOf('class="mermaid"') > -1)
+// assert(mdi.render(`\`\`\`
+// graph TD
+//     A[Christmas] -->|Get money| B(Go shopping)
+//     B ----> C{Let me think}
+//     C -->|One| D[Laptop]
+//     C -->|Two| E[iPhone]
+//     C -->|Three| F[Car]
+// \`\`\``).indexOf('<pre>Parse error') > -1)
