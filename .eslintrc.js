84034ab2e3dc380d5f42dbc949234a8103d1fbc7
Manas Talukdar <manas.talukdar@gmail.com>
2019-08-31 13:56:59 -0700

    plumbing

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    'plugin:prettier/recommended',
+    "plugin:vue/recommended",
+    "eslint:recommended"
+  ],
+  plugins: [
+    'prettier'
+  ],
+  // add your custom rules here
+  rules: {
+    "prettier/prettier": ["error", {
+     "endOfLine":"auto"
+     }
+    ],
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+  },
+  globals: {}
+}
