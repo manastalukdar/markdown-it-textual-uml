a0fe047863374e5767ec9e1dc34d5cd0ce5de033
Manas Talukdar <manas.talukdar@gmail.com>
2019-09-01 19:50:43 -0700

    plantuml tests passing.

--- test/ditaa.js
+++ test/ditaa.js
@@ -1,21 +1,26 @@
 'use strict';
 
-var path = require('path');
-var generate = require('markdown-it-testgen');
+import path from 'path';
+import generate from 'markdown-it-testgen';
 import md from 'markdown-it'
 import umlPlugin from '../src/index.js'
 
 /*eslint-env mocha*/
 
 describe('markdown-it-textual-uml: ditaa - default', function () {
-  it('test ditaa', function(done) {
+
+  it('test ditaa', function() {
     var ditaaParser = md().use(umlPlugin);
 
-    generate(
-      path.join(__dirname, 'fixtures/ditaa/default.txt'),
-      { header: true },
-      ditaaParser
-    );
-    done();
+    function runGenerate() {
+      return Promise.resolve(() => {
+        generate(
+          path.join(__dirname, 'fixtures/ditaa/default.txt'),
+          { header: true },
+          ditaaParser
+        );
+      })
+    }
+    runGenerate()
   });
 });
--- test/plantuml.js
+++ test/plantuml.js
@@ -1,108 +1,58 @@
 'use strict';
 
-//import assert from 'assert';
-//import markdownItMermaid from '../src';
 
-var path = require('path');
-var generate = require('markdown-it-testgen');
+import path from 'path';
+import generate from 'markdown-it-testgen';
 import md from 'markdown-it'
 import umlPlugin from '../src/index.js'
 
 /*eslint-env mocha*/
 
-describe('markdown-it-textual-uml: plantuml - default', function () {
-  it('test default', function(done) {
+describe('markdown-it-textual-uml: plantuml', function () {
+  this.timeout(5000);
+
+  it('test default', function() {
     var defaultParser = md().use(umlPlugin);
 
-    generate(
-      path.join(__dirname, 'fixtures/plantuml/default.txt'),
-      { header: true },
-      defaultParser
-    )
-    done();
+    function runGenerate() {
+      return Promise.resolve(() => {
+        generate(
+          path.join(__dirname, 'fixtures/plantuml/default.txt'),
+          { header: true },
+          defaultParser
+        )
+      })
+    }
+    runGenerate()
   });
-});
 
-describe('markdown-it-textual-uml: plantuml - png', function () {
-  it('test png', function(done) {
+  it('test png', function() {
     var pngParser = md().use(umlPlugin, { imageFormat: 'png' });
 
-    generate(
-      path.join(__dirname, 'fixtures/plantuml/png.txt'),
-      { header: true },
-      pngParser
-    );
-    done();
+    function runGenerate() {
+      return Promise.resolve(() => {
+        generate(
+          path.join(__dirname, 'fixtures/plantuml/png.txt'),
+          { header: true },
+          pngParser
+        );
+      })
+    }
+    runGenerate()
   });
-});
 
-describe('markdown-it-textual-uml: plantuml - server', function () {
-  it('test server', function(done) {
+  it('test server', function() {
     var parserWithCustomServer = md().use(umlPlugin, { server: 'http://example.com/umlPlugin' });
 
-    generate(
-      path.join(__dirname, 'fixtures/plantuml/server.txt'),
-      { header: true },
-      parserWithCustomServer
-    );
-    done();
+    function runGenerate() {
+      return Promise.resolve(() => {
+        generate(
+          path.join(__dirname, 'fixtures/plantuml/server.txt'),
+          { header: true },
+          parserWithCustomServer
+        );
+      })
+    }
+    runGenerate()
   });
 });
-
-
-/* const mdi = markdownIt()
-mdi.use(markdownItMermaid);
-
-assert(mdi.render('# Hello world').trim() === '<h1>Hello world</h1>', '# Hello world')
-assert(mdi.render('Hello world').trim() === '<p>Hello world</p>', 'Hello world')
-
-console.log(mdi.render(`\`\`\`mermaid
-graph TD
-    A[Christmas] -->|Get money| B(Go shopping)
-    B --> C{Let me think}
-    C -->|One| D[Laptop]
-    C -->|Two| E[iPhone]
-    C -->|Three| F[Car]
-\`\`\``))
-*/
-// console.log(mdi.render(`\`\`\`
-// graph TD
-//     A[Christmas] -->|Get money| B(Go shopping)
-//     B --> C{Let me think}
-//     C -->|One| D[Laptop]
-//     C -->|Two| E[iPhone]
-//     C -->|Three| F[Car]
-// \`\`\``))
-// console.log(mdi.render(`\`\`\`
-// graph TD
-//     A[Christmas] -->|Get money| B(Go shopping)
-//     B ----> C{Let me think}
-//     C -->|One| D[Laptop]
-//     C -->|Two| E[iPhone]
-//     C -->|Three| F[Car]
-// \`\`\``))
-
-// assert(mdi.render(`\`\`\`mermaid
-// graph TD
-//     A[Christmas] -->|Get money| B(Go shopping)
-//     B --> C{Let me think}
-//     C -->|One| D[Laptop]
-//     C -->|Two| E[iPhone]
-//     C -->|Three| F[Car]
-// \`\`\``).indexOf('class="mermaid"') > -1)
-// assert(mdi.render(`\`\`\`
-// graph TD
-//     A[Christmas] -->|Get money| B(Go shopping)
-//     B --> C{Let me think}
-//     C -->|One| D[Laptop]
-//     C -->|Two| E[iPhone]
-//     C -->|Three| F[Car]
-// \`\`\``).indexOf('class="mermaid"') > -1)
-// assert(mdi.render(`\`\`\`
-// graph TD
-//     A[Christmas] -->|Get money| B(Go shopping)
-//     B ----> C{Let me think}
-//     C -->|One| D[Laptop]
-//     C -->|Two| E[iPhone]
-//     C -->|Three| F[Car]
-// \`\`\``).indexOf('<pre>Parse error') > -1)
