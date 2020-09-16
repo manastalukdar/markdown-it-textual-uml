fccddfa2d4bffa1ca8143c0cd65638a7fd3aeea7
Manas Talukdar <manas.talukdar@gmail.com>
2020-09-15 23:28:38 -0700

    Added eslint markdown.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,26 +2,53 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
+    es6: true
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
+    "parserOptions": {
+      "ecmaVersion": 6,
+      "sourceType": "module",
+      "ecmaFeatures": {
+          "jsx": true,
+          "modules": true,
+          "experimentalObjectRestSpread": true
+      }
   },
   extends: [
     'plugin:prettier/recommended',
-    "eslint:recommended"
+    "eslint:recommended",
+    'google',
+    'standard'
   ],
   plugins: [
-    'prettier'
+    'prettier',
+    'markdown'
   ],
   // add your custom rules here
   rules: {
-    "prettier/prettier": ["error", {
-     "endOfLine":"auto"
-     }
+    'no-underscore-dangle': 0,
+    'import/imports-first': ['error', 'absolute-first'],
+    'import/newline-after-import': 'error',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto'
+      }
     ],
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
   },
-  globals: {}
+  globals: {
+    window: true,
+    document: true,
+    localStorage: true,
+    FormData: true,
+    FileReader: true,
+    Blob: true,
+    navigator: true,
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  }
 }
